281efc4eaeb34263620acc7c15274ec06a6c1a38
mrpixxo <luckyschaller@gmail.com>
2024-11-06 16:36:19 +0100

    exercise 3 1c

--- poisson.cc
+++ poisson.cc
@@ -1,16 +1,18 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <cmath>
 
 
 double poisson(double mu, int k) {
-    return 0;
+    return pow(mu, k) * exp(-mu) / tgamma(k + 1);
 }
 
 int main() {
     using namespace std;
     vector<int> zaehler(11);
     ofstream fout("hist.txt");
+    ofstream fout2("histpoi.txt");
     ifstream fin("datensumme.txt");
     int n_i;
     for(int i = 0 ; i < 234 ; ++i) {
@@ -19,6 +21,7 @@ int main() {
     }
     for(unsigned int i = 0 ; i < zaehler.size() ; ++i) {
     fout << i << " " << zaehler[i] << endl;
+    fout2 << i << " " << zaehler[i] << " " <<234*poisson(3.11538, i) << endl;
     }
     fin.close();
 }
