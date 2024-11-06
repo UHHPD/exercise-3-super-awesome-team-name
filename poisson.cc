3d83cf52a3aff417d5e5251cb99010701652b716
mrpixxo <luckyschaller@gmail.com>
2024-11-06 16:09:26 +0100

    exercise 1

--- poisson.cc
+++ poisson.cc
@@ -9,12 +9,17 @@ double poisson(double mu, int k) {
 
 int main() {
     using namespace std;
+    vector<int> zaehler(11);
 
 
     ifstream fin("datensumme.txt");
     int n_i;
     for(int i = 0 ; i < 234 ; ++i) {
         fin >> n_i;
+        zaehler[n_i] += 1;
+    }
+    for(unsigned int i = 0 ; i < zaehler.size() ; ++i) {
+        cout << i << ": " << zaehler[i] << endl;
     }
     fin.close();
 }
