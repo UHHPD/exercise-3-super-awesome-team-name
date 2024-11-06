efbe33992fbecb753ac92ae7221bda929aadf78e
mrpixxo <luckyschaller@gmail.com>
2024-11-06 16:46:12 +0100

    sheet 4 exercise 1a

--- likelihood.cc
+++ likelihood.cc
@@ -1,10 +1,17 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
-
+#include <cmath>
 
 double poisson(double mu, int k) {
-    return 0;
+    return pow(mu, k) * exp(-mu) / tgamma(k + 1);
+}
+double prob(std::vector<int> daten, double mu) {
+    double L = 1;
+    for(int i :daten) {
+        L *= poisson(mu, i);
+    }
+    return L;
 }
 
 int main() {
@@ -13,8 +20,12 @@ int main() {
 
     ifstream fin("datensumme.txt");
     int n_i;
+    std::vector<int> daten;
     for(int i = 0 ; i < 234 ; ++i) {
         fin >> n_i;
+        daten.push_back(n_i);
     }
+    double mu = 3.11538;
+    cout << prob(daten, mu) << endl;
     fin.close();
 }
